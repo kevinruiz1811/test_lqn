38eed841a582482aa81118232f288c27fb19f526
kevinruiz1811 <kevin.ruiz1811gmail.com>
2025-09-23 09:28:15 -0500

    Ajuste final de diseño

--- website_lqn/app/layout.tsx
+++ website_lqn/app/layout.tsx
@@ -1,14 +1,20 @@
 import { ReactNode } from "react";
 import ApolloWrapper from "../components/ApolloWrapper";
+import { Orbitron } from "next/font/google";
 
 export const metadata = {
   title: "Star Wars Fans",
 };
 
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body style={{ backgroundColor: "black" }}>
+      <body style={{ backgroundColor: "black", fontFamily: orbitron.style.fontFamily }}>
         <ApolloWrapper>{children}</ApolloWrapper>
       </body>
     </html>
